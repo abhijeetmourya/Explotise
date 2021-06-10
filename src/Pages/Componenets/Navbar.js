f194e8e0f4c83061b633db674bec801d8486914b
Abhijeet Mourya <67868256+abhijeetmourya@users.noreply.github.com>
2021-06-10 10:21:24 +0530

    navbar created

---
+++ src/Pages/Componenets/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+
+
+    return (
+      <header>
+      <section>
+      <a href="#" id="logo" >Explotise</a>
+
+      <label for="toggle-1" className="toggle-menu"><ul><li></li> <li></li> <li></li></ul></label>
+      <input type="checkbox" id="toggle-1" />
+
+      <nav>
+      <ul>
+      <li><a href="#logo"><i className="icon-home"></i>Home</a></li>
+      <li><a href="#about"><i className="icon-user"></i>About</a></li>
+      <li><a href="#services"><i className="icon-compass"></i>Explore</a></li>
+      <li><a href="#contact"><i className="icon-phone"></i>Contact</a></li>
+      </ul>
+      </nav>
+      </section>
+      </header>
+    )
+
+}
+
+export default Navbar;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import './Home.css';
+import React from 'react'
+import Navbar from '../Componenets/Navbar'
 
 function Home() {
     return (
-        <section className="navbar">
-            <div className="brand-title">Explore</div>
-            <div className="navbar-links">
-                <a href="#" class="toggle-button">
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                    <span class="bar"></span>
-                 </a>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Explore</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </div>
-        </section>
+        <div>
+            <Navbar />
+        </div>
     )
 }
 
